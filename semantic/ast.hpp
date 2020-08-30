2817d970a98d39bfee86d2e0ee0bcbbfd4f90221
pamin98 <minaidispanagiotis@yahoo.gr>
2020-08-30 19:14:51 +0300

    Init semantic analysis. Code doesn't compile yet.

---
+++ semantic/ast.hpp
@@ -0,0 +1,604 @@
+#pragma once
+
+#include <iostream>
+#include <map>
+#include <vector>
+#include <string>
+#include <algorithm>
+
+/********************************************************************
+			HEY! CHECK OUT THE "SEM COMPLETED" SECTION!
+ ********************************************************************/
+
+enum relOp { eq, lt, gt, le, ge, neq };
+
+enum logOp { AND, OR, NOT, TRUE, FALSE };
+
+enum listOp { nil, nilq, head, tail, append };
+
+typedef enum HeaderType { Func_Decl, Func_Def } HeaderType;
+
+class AST
+{
+public:
+	virtual ~AST() {}
+	virtual void printOn(std::ostream &out) const = 0;
+	virtual void sem () {}
+};
+
+inline std::ostream &operator<<(std::ostream &out, const AST &t)
+{
+	t.printOn(out);
+	return out;
+}
+
+class Expr : public AST
+{
+public:
+	//virtual int eval() const = 0;
+	void type_check(Type t) {
+		sem();
+		if (type != t) error("Type mismatch.");
+	}
+protected:
+	Type type;
+};
+
+class Stmt : public AST
+{
+public:
+	//virtual void run() const = 0;
+};
+
+class ExitStmt : public Stmt
+{
+public:
+	ExitStmt() {}
+	~ExitStmt() {}
+	virtual void printOn(std::ostream &out) const override
+	{
+		out << "Exit";
+	}
+};
+
+class ReturnStmt : public Stmt
+{
+public:
+	ReturnStmt(Expr *e) : returnExpr(e) {}
+	~ReturnStmt() {}
+	virtual void printOn(std::ostream &out) const override
+	{
+		out << "Return";
+	}
+
+private:
+	Expr *returnExpr;
+};
+
+class SkipStmt : public Stmt
+{
+public:
+	SkipStmt() {}
+	~SkipStmt() {}
+	virtual void printOn(std::ostream &out) const override
+	{
+		out << "Skip";
+	}
+};
+
+extern std::map<std::string, int> globals;
+
+class Var : public Expr
+{
+public:
+	Var(std::string v, std::string t) : var(v), type(t) {}
+	virtual void printOn(std::ostream &out) const override
+	{
+		out << "Id(" << type << " " << var << ")";
+	}
+
+	virtual void sem() override {
+
+	}
+
+private:
+	std::string var;
+	std::string type;
+};
+
+
+class ConstInt : public Expr
+{
+public:
+	ConstInt(int n) : num(n) {}
+	virtual void printOn(std::ostream &out) const override
+	{
+		out << "Const(" << num << ")";
+	}
+
+private:
+	int num;
+};
+
+class ConstString : public Expr
+{
+public:
+	ConstString(std::string str) : str(str) {}
+	virtual void printOn(std::ostream &out) const override
+	{
+		out << "Const(" << str << ")";
+	}
+
+private:
+	std::string str;
+};
+
+class BinOp : public Expr
+{
+public:
+	BinOp(Expr *l, char o, Expr *r) : left(l), op(o), right(r) {}
+	~BinOp()
+	{
+		delete left;
+		delete right;
+	}
+	virtual void printOn(std::ostream &out) const override
+	{
+		out << op << "(" << *left << ", " << *right << ")";
+	}
+
+private:
+	Expr *left;
+	char op;
+	Expr *right;
+};
+
+class RelOp : public Expr
+{
+public:
+	RelOp(Expr *l, relOp o, Expr *r) : left(l), op(o), right(r) {}
+	~RelOp()
+	{
+		delete left;
+		delete right;
+	}
+	virtual void printOn(std::ostream &out) const override
+	{
+		out << op << "(" << *left << ", " << *right << ")";
+	}
+
+private:
+	Expr *left;
+	relOp op;
+	Expr *right;
+};
+
+class LogOp : public Expr
+{
+public:
+	LogOp(logOp o, Expr *l = NULL, Expr *r = NULL) : left(l), op(o), right(r) {}
+	~LogOp()
+	{
+		delete left;
+		delete right;
+	}
+	virtual void printOn(std::ostream &out) const override
+	{
+		out << op << "(" << *left << ", " << *right << ")";
+	}
+
+private:
+	Expr *left;
+	logOp op;
+	Expr *right;
+};
+
+class ListOp : public Expr
+{
+public:
+	ListOp(listOp o, Expr *l = NULL, Expr *r = NULL) : left(l), op(o), right(r) {}
+	~ListOp()
+	{
+		delete left;
+		delete right;
+	}
+	virtual void printOn(std::ostream &out) const override
+	{
+		out << op << "(" << *left << ", " << *right << ")";
+	}
+
+private:
+	Expr *left;
+	listOp op;
+	Expr *right;
+};
+
+
+class StmtList : public Stmt
+{
+public:
+	StmtList(Stmt *s)
+	{
+		stmt_list.push_back(s);
+	}
+	~StmtList()
+	{
+		for (Stmt *s : stmt_list)
+			delete s;
+	}
+	void append(Stmt *s) { stmt_list.push_back(s); }
+
+	virtual void printOn(std::ostream &out) const override
+	{
+		out << "StmtList(";
+		bool first = true;
+		for (Stmt *s : stmt_list)
+		{
+			if (!first)
+				out << ", ";
+			first = false;
+			out << *s;
+		}
+		out << ")";
+	}
+
+private:
+	std::vector<Stmt *> stmt_list;
+};
+
+class For : public Stmt
+{
+public:
+	For( StmtList *s1 ,Expr *e, StmtList *s2 , StmtList *s3) : 
+	initializers(s1) , threshold(e) , steps(s2) , loop_body(s3) {}
+	~For()
+	{
+		delete threshold;
+		delete initializers;
+		delete steps;
+		delete loop_body;
+	}
+	virtual void printOn(std::ostream &out) const override
+	{
+		//out << "For(" << *initializers << ", " << *threshold << ", " *steps << ": " << *loop_body << ")";
+	}
+
+private:
+	Expr *threshold;
+	Stmt *initializers;
+	Stmt *steps;
+	Stmt *loop_body;
+};
+
+class ExprList : public Expr
+{
+public:
+	ExprList() : expr_list() {}
+	~ExprList()
+	{
+		for (Expr *e : expr_list)
+			delete e;
+	}
+	void append(Expr *e) { expr_list.push_back(e); }
+
+	virtual void printOn(std::ostream &out) const override
+	{
+		out << "ExprList(";
+		bool first = true;
+		for (Expr *e : expr_list)
+		{
+			if (!first)
+				out << ", ";
+			first = false;
+			out << *e;
+		}
+		out << ")";
+	}
+
+private:
+	std::vector<Expr *> expr_list;
+};
+
+class If : public Stmt
+{
+public:
+	If(Expr *c, StmtList *s, If *next = NULL) : cond(c), stmt_list(s), nextIf(next) {}
+	~If()
+	{
+		delete cond;
+		delete stmt_list;
+		delete nextIf;
+	}
+	virtual void printOn(std::ostream &out) const override
+	{
+		//out << "If(" << *cond << ", " << stmt_list->printOn(out) << ")";
+	}
+	void append(If *i){	nextIf = i;	}
+
+private:
+	Expr *cond;
+	StmtList *stmt_list;
+	If *nextIf;
+};
+
+class ArrayIndexing : public Expr
+{
+public:
+	ArrayIndexing(Expr *a, Expr *i) : array(a), index(i) {}
+	~ArrayIndexing()
+	{
+		delete array;
+		delete index;
+	}
+	virtual void printOn(std::ostream &out) const override
+	{
+		out << "(" << *array << ", " << *index << ")";
+	}
+
+private:
+	Expr *array;
+	Expr *index;
+};
+
+class CallObject : public Expr, public Stmt
+{
+public:
+	CallObject(Var *n, ExprList *e) : functionName(n), expr_list(e) {}
+	~CallObject()
+	{
+		delete functionName;
+		delete expr_list;
+	}
+	virtual void printOn(std::ostream &out) const override
+	{
+		out << "(" << *functionName << ", " << *expr_list << ")";
+	}
+
+private:
+	Var *functionName;
+	ExprList *expr_list;
+};
+
+class AssignStmt : public Stmt
+{
+public:
+	AssignStmt(Expr *l, Expr *r) : left(l), right(r) {}
+	~AssignStmt()
+	{
+		delete left;
+		delete right;
+	}
+	virtual void printOn(std::ostream &out) const override
+	{
+		//out << "If(" << *cond << ", " << stmt_list->printOn(out) << ")";
+	}
+
+private:
+	Expr *left;
+	Expr *right;
+};
+
+class ArrayInit : public Expr
+{
+public:
+	ArrayInit(std::string *t , Expr *e) : type(t) , expr(e) {};
+	~ArrayInit()
+	{
+		delete type;
+		delete expr;
+	}
+	virtual void printOn(std::ostream &out) const override
+	{
+		out << type ;
+	}
+private:
+	std::string *type;
+	Expr *expr;
+};
+
+
+
+class Formal : public AST
+{
+public:
+	Formal(std::string *t, VarList *v, bool isRef=false) : type(t) , var_list(v), isRef(isRef) {}
+	~Formal() 
+	{
+		delete var_list; 
+		delete type;
+	}
+
+	virtual void printOn(std::ostream &out) const override
+	{
+		
+	}
+
+private:
+	std::string *type;
+	VarList *var_list;
+	bool isRef;
+};
+
+class FormalList : public AST
+{
+public:
+	FormalList() {}
+	~FormalList() {}
+
+	void append(Formal *f){	formal_list.push_back(f); }
+
+	virtual void printOn(std::ostream &out) const override
+	{
+		
+	}
+
+private:
+	std::vector<Formal *> formal_list;
+};
+
+
+/****************************************************************************************************************
+ ************************************************ SEM COMPLETED *************************************************
+ ***************************************************************************************************************/
+
+class Header : public AST
+{
+public:
+	Header(const char *v, FormalList *fl, Type t, HeaderType D) : 
+	functionName(v), formal_list(fl) , type(t), T(D) {} 
+	~Header()
+	{
+		delete functionName;
+		delete formal_list;
+		delete type;
+	}
+
+	virtual void printOn(std::ostream &out) const override
+	{
+		
+	}
+
+	virtual void sem () override {
+		/* newFunction performs check for duplicate declaration in the same scope */
+		SymbolEntry * f = newFunction(functionName);
+		if(!T)		/* Declarations should be forwarded */
+			forwardFunction(f);
+		openScope()
+		formal_list->sem();
+		endFunctionHeader(f, type);
+		closeScope();
+	}
+
+private:
+	const char *functionName;
+	FormalList *formal_list;
+	Type type;
+	HeaderType T;
+};
+
+class Definition : public AST {
+};
+
+class DefinitionList : public AST
+{
+public:
+	DefinitionList() : definition_list() {}
+	~DefinitionList(){}
+
+	void append(Definition *d){	definition_list.push_back(d); }
+
+
+	virtual void printOn(std::ostream &out) const override
+	{
+		
+	}
+
+	virtual void sem() override{
+		for (Definition *d : definition_list) d->sem();
+	}
+private:
+	std::vector<Definition *> definition_list;
+};
+
+class FunctionDefinition : public Definition
+{
+public:
+	FunctionDefinition(Header *h,DefinitionList *dl=NULL, StmtList *sl=NULL) : header(h), def_list(dl), stmt_list(sl) {} 
+	~FunctionDefinition()
+	{
+		delete header;
+		delete def_list;
+		delete stmt_list;
+	}
+
+	void setDefinitionList(DefinitionList *dl)
+	{
+		def_list = dl;
+	}
+
+	void setStmtList(StmtList *sl)
+	{
+		stmt_list = sl;
+	}
+
+	virtual void printOn(std::ostream &out) const override
+	{
+		
+	}
+
+	virtual void sem () override {
+		openScope();
+		header->sem();
+		def_list->sem();
+		stmt_list->sem();
+		closeScope();
+	} 
+private:
+	Header *header;
+	DefinitionList *def_list;
+	StmtList *stmt_list;
+};
+
+class FunctionDeclaration : public Definition
+{
+public:
+	FunctionDeclaration(Header *h) : header(h) {} 
+	~FunctionDeclaration()
+	{
+		delete header;
+	}
+
+	virtual void printOn(std::ostream &out) const override
+	{
+		
+	}
+
+	virtual void sem () override {
+		openScope();
+		header->sem();
+		closeScope();
+	}
+private:
+	Header *header;
+};
+
+class VarList : public Expr
+{
+public:
+	std::vector<const char *> var_list;
+	
+	VarList(const char *v)	{ var_list.push_back(v); }
+	
+	void append(const char *v)	{ var_list.push_back(v); }
+
+	virtual void printOn(std::ostream &out) const override
+	{
+		//out << "Id(" << type << " " << var << ")";
+	}
+ 
+};
+
+class VarDefinition : public Definition
+{
+public:
+	VarDefinition(Type t, VarList *v) : type(t), var_list(v) {}
+	~VarDefinition() 
+	{
+		delete var_list; 
+		delete type;
+	}
+
+	virtual void printOn(std::ostream &out) const override
+	{
+		
+	}
+
+	virtual void sem() override {
+		for (const char * variable : var_l->var_list)
+			SymbolEntry * e = newVariable(variable, type);
+	}
+
+private:
+	Type type;
+	VarList *var_l;
+};
---
+++ semantic/general.hpp
@@ -0,0 +1,29 @@
+/******************************************************************************
+ *  CVS version:
+ *     $Id: general.h,v 1.1 2004/05/05 22:00:08 nickie Exp $
+ ******************************************************************************
+ *
+ *  C header file : general.h
+ *  Project       : PCL Compiler
+ *  Version       : 1.0 alpha
+ *  Written by    : Nikolaos S. Papaspyrou (nickie@softlab.ntua.gr)
+ *  Date          : May 5, 2004
+ *  Description   : Generic symbol table in C, general header file
+ *  Modified by	  : Minaidis P., Kimonides A.
+ *  
+ */
+
+
+#ifndef __GENERAL_HPP__
+#define __GENERAL_HPP__
+
+/*
+void * new    (size_t);
+void   delete (void *);
+*/
+
+extern int lineno;
+extern const char * filename;
+
+
+#endif
