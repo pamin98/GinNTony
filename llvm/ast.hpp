a119f1c5a841cb0a4685dfd437ac3d0f48bc286e
akimonid <akimonid@redhat.com>
2021-07-21 16:29:42 +0300

    fix makefile

--- llvm/ast.hpp
+++ llvm/ast.hpp
@@ -265,19 +265,22 @@ public:
 		TheFPM->add(llvm::createPromoteMemoryToRegisterPass());
 		TheFPM->add(llvm::createMemCpyOptPass());
 		TheFPM->add(llvm::createInstructionCombiningPass());
+		// MINE 
+		TheFPM->add(llvm::createDeadCodeEliminationPass());
+		// ----
 		TheFPM->add(llvm::createReassociatePass());
 		TheFPM->add(llvm::createGVNPass());
 		TheFPM->add(llvm::createCFGSimplificationPass());
 		TheFPM->add(llvm::createCorrelatedValuePropagationPass());
 		TheFPM->add(llvm::createDeadCodeEliminationPass());
-		TheFPM->add(llvm::createDeadArgEliminationPass());
-		TheFPM->add(llvm::createConstantMergePass());
-		TheFPM->add(llvm::createGlobalDCEPass());
-		TheFPM->add(llvm::createGlobalOptimizerPass());
+		// TheFPM->add(llvm::createDeadArgEliminationPass());
+		// TheFPM->add(llvm::createConstantMergePass());
+		// TheFPM->add(llvm::createGlobalDCEPass());
+		// TheFPM->add(llvm::createGlobalOptimizerPass());
 		TheFPM->add(llvm::createGVNPass());
 		TheFPM->add(llvm::createIndVarSimplifyPass());
 		TheFPM->add(llvm::createInstructionCombiningPass());
-		TheFPM->add(llvm::createFunctionInliningPass());
+		// TheFPM->add(llvm::createFunctionInliningPass());
 		TheFPM->add(llvm::createJumpThreadingPass());
 		TheFPM->add(llvm::createLICMPass());
 		TheFPM->add(llvm::createLoopDeletionPass());
@@ -285,7 +288,7 @@ public:
 		TheFPM->add(llvm::createLoopUnrollPass());
 		TheFPM->add(llvm::createLoopUnswitchPass());
 		TheFPM->add(llvm::createSCCPPass());
-		TheFPM->add(llvm::createStripDeadPrototypesPass());
+		// TheFPM->add(llvm::createStripDeadPrototypesPass());
 		TheFPM->add(llvm::createTailCallEliminationPass());
 		
 
